bd0371fa059032eb5a4e5892b213302008996e2a
Paola Calle <pcalle@smith.edu>
2024-11-15 14:47:44 -0500

    minor

---
+++ DNS_local_resolver.py
@@ -0,0 +1,13 @@
+from dnslib import DNSRecord, DNSError, QTYPE, RCODE, RR
+
+# This is a simple DNS resolver that can resolve certain types of DNS queries.
+
+class DNSLocalResolver(object):
+    """
+    This class is a simple DNS resolver that can resolve certain types of DNS queries.
+    It is designed to be used with an object of the class.
+    """
+    
+    
+    
+    
--- dns-resolver.py
+++
@@ -1,3 +0,0 @@
-from dnslib import DNSRecord, DNSError, QTYPE, RCODE, RR
-
-# This is a simple DNS resolver that can resolve certain types of DNS queries.
