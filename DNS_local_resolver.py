2bd03daf4411f3b9ead5c9c0922da6c1b2dc6513
lsavitsky <lsavitsky@smith.edu>
2024-11-15 14:48:53 -0500

    Ref to root file

--- DNS_local_resolver.py
+++ DNS_local_resolver.py
@@ -1,3 +1,6 @@
+"""
+Refence: https://www.iana.org/domains/root/files    - for the root file
+"""
 from dnslib import DNSRecord, DNSError, QTYPE, RCODE, RR
 
 # This is a simple DNS resolver that can resolve certain types of DNS queries.
