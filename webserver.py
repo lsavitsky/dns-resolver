37892ff1891ef8fc22f0bb190d2ee55e82e3d3be
lsavitsky <lsavitsky@smith.edu>
2024-11-15 14:18:21 -0500

    Basic http Server and html file

---
+++ webserver.py
@@ -0,0 +1,24 @@
+#http_server.py
+
+import http.server
+import socketserver
+
+PORT = 8080 # Designation 
+DIRECTORY = "html_files"
+# Creates a HTTP Server hosting pur html file
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler): 
+    def do_GET(self):
+        if self.path == '/a.com':
+            self.path = '/a.html'
+        elif self.path == '/b.com':
+            self.path = '/b.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+    def translate_path(self, path):
+        return f"./{DIRECTORY}/{path}"
+
+handler_object = MyHttpRequestHandler
+my_server = socketserver.TCPServer(("0.0.0.0", PORT), handler_object)
+
+print(f"Serving HTTP on port {PORT}")
+my_server.serve_forever()
