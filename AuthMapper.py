54a630087d1fd4d3bc2dce9906ec4caa32940cb1
Paola Calle <pcalle@smith.edu>
2024-11-18 16:57:22 -0500

    authmapper enumeration

--- AuthMapper.py
+++ AuthMapper.py
@@ -1,31 +1,66 @@
 from enum import Enum
 from collections import defaultdict
 
+file = "../domain/redpanda.cache"
 class AuthMapper(Enum):
     
     def __init__(self) -> None:
-        self.file="/domain/redpanda.cache"
         self.map = self.create_dyn_enum()
         
-
-    def read_cache_save_dic(self):
+    @staticmethod
+    def read_cache_save_dic(file : str):
         res = defaultdict(dict) # initialize dic of records
 
-        with open (self.file, 'r') as feed:
-            line = feed.readline()
-            if line[0]!=';':
-                lineList=line.split(" ")
-                rootserver, num, record, IPdest= lineList[0], lineList[1], lineList[2], lineList[3]
-                res[rootserver][record] = (IPdest) # each record is a key in corresponding rootserver
-                res[rootserver]['num'] = num # extra num attribute
+        with open (file, 'r') as feed:
+            while True:
+                line = feed.readline()
+                if not line:
+                    break
+                
+                if line[0] != ';' and  line[0] != '.':
+                    lineList=line.split()
+                    # print(lineList)
+                    rootserver, num, record, IPdest = lineList[0], lineList[1], lineList[2], lineList[3]
+                    
+                    if res[rootserver].get('record'):
+                        res[rootserver]['record'][record] = IPdest
+                    else:
+                        res[rootserver]['record'] = {record: IPdest} # initialize it
+                        res[rootserver]['num'] = num # only need to set it once
 
         return res
     
-    def create_dyn_enum(self):
-        res = self.read_cache_save_dic()
-        key = res.keys()[0]
-        return Enum(key, res[key])
+    def create_dyn_enum(file : str):
+        res = AuthMapper.read_cache_save_dic(file)
+        res2 = {}
+        
+        for key, value in res.items():
+            print(key, value['record'], value['num'])
+            record = Enum('record', ((key, val) for key, val in value['record'].items()))
+            num = Enum('num', ('num', str(value['num'])))
+            #print("A RECORD:", record.AAAA.value) #EXAMPLE DONT DELETE
+            print("enum stuff: ", Enum('record', ((key, val) for key, val in value['record'].items())))
+            print("num stuff", num.num.value)
+            res2[key] = Enum('AuthMapper', [record, num]) # add root servers to the hashmap
+            
+            # # first 
+            # ls = []
+            
+            # for innerKey, innerValue in value.items():
+            #     if innerKey == 'num':
+            #         ls.append(Emum('num', innerValue))
+            #     else:
+            #         ls.append(Emum('record', innerValue))
+                    
+            
+        print(list(res2["A.ROOT-SERVERS.NET."]))
+        return res2
     
+cacheDic = AuthMapper.create_dyn_enum( file)
+
+print(cacheDic['A.ROOT-SERVERS.NET.'])
+
+
 
 
 
--- tests/TestAuthMapper.py
+++ tests/TestAuthMapper.py
@@ -4,19 +4,22 @@ import logging
 # from unittest.mock import patch
 # import io
 
-sys.path.insert(0, '../')
+sys.path.append('../')
 import AuthMapper
 
 class TestAuthMapper(unittest.TestCase):
 
     def SetUp(self):
-        self.mapper1 = AuthMapper()
+        self.mapper1 = AuthMapper.AuthMapper()
 
     def TestFirst(self):
         actual = self.mapper1(1).map
         logging.basicConfig(level = logging.INFO)
         logging.info(actual)
         
-
+        
+mapper1 = AuthMapper.AuthMapper()       
+# actual = mapper1(1).map
+# print(actual)
 if __name__ == '__main__':
     unittest.main()
