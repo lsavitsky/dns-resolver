07c50ad0fec38f2e44c488c4aa7bd4bdac6a79c4
lsavitsky <113080111+lsavitsky@users.noreply.github.com>
2024-12-08 15:32:49 -0500

    Minoradds to Authoriative DNS Resolver

--- dns_servers/DNS_authoritative_resolver.py
+++ dns_servers/DNS_authoritative_resolver.py
@@ -1,4 +1,9 @@
+from dns_resolver import DNSResolver
+from pathlib import Path
 
-
-class AuthoritativeDNSResolver:
-    pass # implement this class
+class DNS_Authoritative_resolver(DNSResolver):
+    """
+    Authoritative DNS resolver for a specific zone.
+    """
+    def __init__(self, file: Path):
+        super().__init__(file)
