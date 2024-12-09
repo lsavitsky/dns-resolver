2650e3cc3d54c5760d62889e84ae10eb45e9f15d
lsavitsky <113080111+lsavitsky@users.noreply.github.com>
2024-12-09 05:34:09 -0500

    Stuff

--- dns_servers/DNS_Query/dns_construction/dns_packet.py
+++ dns_servers/DNS_Query/dns_construction/dns_packet.py
@@ -1,2 +1,20 @@
-class dns_header:
-    pass
+from dns_question_enum import q_construction
+from dns_header_construction import dns_header
+class dns_packet:
+    """
+    Constructs a full DNS packet by combining the header and question section.
+    """
+    def __init__():
+        q=q_construction()
+        header= dns_header()
+        
+    def construct_packet(self) -> bytes:
+        """
+        Combine the header and question section into a full DNS packet.
+
+        :return: The full DNS packet as bytes.
+        """
+        # Serialize the header and question section
+        header_bytes = self.header.byte_format_construction()
+        question_bytes = self.question.construct()
+        return header_bytes + question_bytes
